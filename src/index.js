d2f78a837bbea598e66a0e818fdded28cab2d1cc
Anoop Varghese <anupvarghese@gmail.com>
2017-04-01 19:57:51 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Paper extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        PaperJS with React
+      </div>
+    );
+  }
+}
+
+export default Paper;
