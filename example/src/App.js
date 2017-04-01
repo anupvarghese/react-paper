43d47711b393c31e9db204e2a19fa6c010662a13
Anoop Varghese <anupvarghese@gmail.com>
2017-04-01 20:06:17 +1100

    add example

---
+++ example/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactPaper from '../../lib/index';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-intro">
+          <ReactPaper />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
